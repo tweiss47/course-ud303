333cc7d3b39d956660c0946475f56d83433d85d8
Taylor Weiss <taylor.weiss@comcast.net>
2018-09-24 08:51:39 -0700

    Include content length

--- Lesson-2/0_HelloServer/HelloServer.py
+++ Lesson-2/0_HelloServer/HelloServer.py
@@ -9,15 +9,19 @@ from http.server import HTTPServer, BaseHTTPRequestHandler
 
 class HelloHandler(BaseHTTPRequestHandler):
     def do_GET(self):
-        # First, send a 200 OK response.
+        # First determine the response body
+        body = "Hello, HTTP!\n".encode()
+
+        # Send a 200 OK response.
         self.send_response(200)
 
         # Then send headers.
         self.send_header('Content-type', 'text/plain; charset=utf-8')
+        self.send_header('Content-length', len(body))
         self.end_headers()
 
         # Now, write the response body.
-        self.wfile.write("Hello, HTTP!\n".encode())
+        self.wfile.write(body)
 
 if __name__ == '__main__':
     server_address = ('', 8000)  # Serve on all addresses, port 8000.
