81bb1119618956328bbb5b03481623cd359398be
Taylor Weiss <taylor.weiss@comcast.net>
2018-09-24 13:40:30 -0700

    Implement the path echo server

--- Lesson-2/1_EchoServer/EchoServer.py
+++ Lesson-2/1_EchoServer/EchoServer.py
@@ -19,19 +19,27 @@
 from http.server import HTTPServer, BaseHTTPRequestHandler
 
 
-class HelloHandler(BaseHTTPRequestHandler):
+class EchoHandler(BaseHTTPRequestHandler):
     def do_GET(self):
-        # First, send a 200 OK response.
+        # Build the response
+        body = self.path
+        if len(body) > 0:
+            body = body[1:]
+        body += '\n'
+        body = body.encode()
+
+        # Send the response code
         self.send_response(200)
 
         # Then send headers.
         self.send_header('Content-type', 'text/plain; charset=utf-8')
+        self.send_header('Content-length', len(body))
         self.end_headers()
 
         # Now, write the response body.
-        self.wfile.write("Hello, HTTP!\n".encode())
+        self.wfile.write(body)
 
 if __name__ == '__main__':
     server_address = ('', 8000)  # Serve on all addresses, port 8000.
-    httpd = HTTPServer(server_address, HelloHandler)
+    httpd = HTTPServer(server_address, EchoHandler)
     httpd.serve_forever()
