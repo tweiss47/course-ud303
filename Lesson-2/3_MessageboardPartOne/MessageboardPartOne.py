c382da6a433a634b756fbe1887c87ba6ec3c2c2e
Taylor Weiss <taylor.weiss@comcast.net>
2018-09-25 13:48:21 -0700

    Implement part wone of the messsageboard server

--- Lesson-2/3_MessageboardPartOne/MessageboardPartOne.py
+++ Lesson-2/3_MessageboardPartOne/MessageboardPartOne.py
@@ -24,16 +24,20 @@ from urllib.parse import parse_qs
 class MessageHandler(BaseHTTPRequestHandler):
     def do_POST(self):
         # 1. How long was the message? (Use the Content-Length header.)
+        length = int(self.headers.get('Content-length', 0))
 
         # 2. Read the correct amount of data from the request.
+        data = self.rfile.read(length).decode()
 
         # 3. Extract the "message" field from the request data.
+        message = parse_qs(data)['message'][0].encode()
 
         # Send the "message" field back as the response.
         self.send_response(200)
         self.send_header('Content-type', 'text/plain; charset=utf-8')
+        self.send_header('Content-length', len(message))
         self.end_headers()
-        self.wfile.write(message.encode())
+        self.wfile.write(message)
 
 if __name__ == '__main__':
     server_address = ('', 8000)
